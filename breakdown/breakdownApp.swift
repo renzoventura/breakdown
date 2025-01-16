9cb20d3d041bd250166b8bcf1ea32d333946a5de
Renzo <edrickventura1996@gmail.com>
2025-01-16 14:20:53 +1100

    Initial project commit

---
+++ breakdown/breakdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  breakdownApp.swift
+//  breakdown
+//
+//  Created by Renzo on 16/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct breakdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ breakdownTests/breakdownTests.swift
@@ -0,0 +1,17 @@
+//
+//  breakdownTests.swift
+//  breakdownTests
+//
+//  Created by Renzo on 16/1/2025.
+//
+
+import Testing
+@testable import breakdown
+
+struct breakdownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
