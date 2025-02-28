2f0b7e5e6b8d815ab69f20e3b390d6666e73c785
Renzo <edrickventura1996@gmail.com>
2025-03-01 01:00:41 +1100

    Pass the currentSelectedTask to viewModel instead of directly passing to the new view

---
+++ breakdown/Screens/TaskDetail/Components/ListOfSubTasks.swift
@@ -0,0 +1,18 @@
+//
+//  ListOfSubTasks.swift
+//  breakdown
+//
+//  Created by Renzo on 1/3/2025.
+//
+
+import SwiftUI
+
+struct ListOfSubTasks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListOfSubTasks()
+}
