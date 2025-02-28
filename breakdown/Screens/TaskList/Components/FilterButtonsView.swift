fedd80cdd97ce7ca9885f94aa399eb6f662e29b4
Renzo <edrickventura1996@gmail.com>
2025-02-28 16:00:18 +1100

    Add toggle filter

---
+++ breakdown/Screens/TaskList/Components/FilterButtonsView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterButtonsView.swift
+//  breakdown
+//
+//  Created by Renzo on 28/2/2025.
+//
+
+import SwiftUI
+
+struct FilterButtonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterButtonsView()
+}
---
+++ breakdown/Screens/TaskList/Components/NoTaskYetView.swift
@@ -0,0 +1,18 @@
+//
+//  NoTaskYetView.swift
+//  breakdown
+//
+//  Created by Renzo on 28/2/2025.
+//
+
+import SwiftUI
+
+struct NoTaskYetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoTaskYetView()
+}
